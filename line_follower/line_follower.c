af05e5d0d5abdefef54d6c4eabed5917b0c944ce
msteinke <martin.steinke@gmail.com>
2014-07-01 15:52:04 +1200

    line follower repo
    I've made a new repo that is more compatable with Atmel Studio

---
+++ line_follower/line_follower.c
@@ -0,0 +1,30 @@
+/*
+ * line_follower.c
+ *
+ * Created: 1/07/2014 3:42:31 p.m.
+ *  Author: martin
+ */
+
+
+#include <avr/io.h>
+#include "system.h"
+
+int main(void)
+{
+	system_init ();
+ 
+	DDRD = 0xFF;
+ 
+	volatile long i = 0;
+ 
+	while(1)
+	{
+		//TODO:: Please write your application code
+		PORTD++;
+ 
+		for (i = 0; i < 90000; i++)
+		{
+			continue;
+		}
+	}
+}
