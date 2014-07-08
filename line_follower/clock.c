c4b1c1e08cedc2a42f0d06995a6ec41b2bed7f0e
Martin Steinke <mcs108@uclive.ac.nz>
2014-07-08 21:52:44 +1200

    worked on clock module. Gonna test if tomorrow.

--- line_follower/clock.c
+++ line_follower/clock.c
@@ -5,40 +5,95 @@
  *  Author: martin
  */ 
 
+#include "clock.h"
+#include "system.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-/** Initialize PWM channels on timer1;
-	PC5 and PC6.
-	@param none
-	@return none */
-/*void motor_init_timer1(void)
+/* Global variable to hold the time
+ max size is 2^32 - 1 = 4294967295 ms
+                      = 4294967 s
+					  = 71582 minutes
+					  = 1193 hours
+					  = 49 days.
+ */
+static volatile unsigned long g_time_ms;
+
+ISR(TIMER1_COMPA_vect)
 {
-	// Configure PWM 
-	// pins: OC.1A & OC.1B
-	//DDRC |= (1<<PC6) | (1<<PC5); 
+	g_time_ms++;
+}
+
+void clock_set_ms(unsigned long time)
+{
+	g_time_ms = time;
+}
+
+void clock_init(void)
+{
+	// clear the time
+	clock_set_ms(0);
+	
+	// Configure PWM timer1
 	DDRC |= (1<<6) | (1<<5); 
 	
 	TCCR1A = (1<<COM1A1) |
 			(0<<COM1A0) |
-			(1<<COM1B1) |
+			(0<<COM1B1) |
 			(0<<COM1B0) |
 			(0<<COM1C1) |
 			(0<<COM1C0) |
-			(0<<WGM11) | // PWM phase correct 8bit mode 1
-			(1<<WGM10);  // PWM phase correct 8bitmode 1
+			(0<<WGM11) | // CTC mode 4
+			(0<<WGM10);  // CTC mode 4
 	
 	TCCR1B = //(0<<FOC0A) | // must disable these for PWM
 			//(0<<FOC0B) | // must disable these for PWM
-			(0<<WGM13) |
-			(0<<WGM12) | // PWM phase correct 8bitmode 1
+			(0<<WGM13) | // CTC mode 4
+			(1<<WGM12) | // CTC mode 4
 			(0<<CS12) | // divide clock by 64
 			(1<<CS11) | // divide clock by 64
 			(1<<CS10); //divide clock by 64
 	
-	// write to the lower half of the 16 bit register.
-	OCR1AL = MOTOR_INIT_DUTY_CYCLE; // PWM duty cycle on PB7 %85
-	OCR1BL = MOTOR_INIT_DUTY_CYCLE; // PWM duty cycle on PD0 %170
+	//TIMSK1 = 0x00; // disable interrupts
+	TIMSK1 = (0<<ICIE1) | // Timer/Countern, Input Capture Interrupt Enable
+			(0<<OCIE1C) | // Timer/Countern, Output Compare C Match Interrupt Enable
+			(0<<OCIE1B) | // Timer/Countern, Output Compare B Match Interrupt Enable
+			(1<<OCIE1A) | // Timer/Countern, Output Compare A Match Interrupt Enable
+			(0<<TOIE1); // Timer/Countern, Overflow Interrupt Enable
 	
-	TIMSK1 = 0x00; // disable interrupts
-	//TIMSK1 = 0x03; // enable interrupts
+	OCR1A = CLOCK_TIMER1_COMPARE_VALUE;
+	//OCR1A = (unsigned short) (F_CPU / CLOCK_TIMER1_PRESCALER) / CLOCK_RATE_HZ;
+	//OCR1A = 125;
+	//OCR1AL = 125;
 	
-}*/
+}
+
+unsigned long clock_get_ms(void)
+{
+	return g_time_ms;
+}
+
+
+/*
+from table 11-1, Reset and Interrupt Vectors:
+16 $001E TIMER1 COMPA Timer/Counter1 Compare Match A
+
+
+TIMSK1 - Timer/Counter1 Interrupt Mask Register
+		ICIEn:  Timer/Countern, Input Capture Interrupt Enable
+		OCIEnC: Timer/Countern, Output Compare C Match Interrupt Enable
+		OCIEnB: Timer/Countern, Output Compare B Match Interrupt Enable
+		OCIEnA: Timer/Countern, Output Compare A Match Interrupt Enable
+		TOIEn:  Timer/Countern, Overflow Interrupt Enable
+		
+		
+		
+		
+TIFR1 - Timer/Counter1 Interrupt Flag Register
+		ICFn:  Timer/Countern, Input Capture Flag
+		OCFnC: Timer/Countern, Output Compare C Match Flag
+		OCFnB: Timer/Counter1, Output Compare B Match Flag
+		OCF1A: Timer/Counter1, Output Compare A Match Flag
+		TOVn:  Timer/Countern, Overflow Flag
+		
+*/
--- line_follower/clock.h
+++ line_follower/clock.h
@@ -11,21 +11,39 @@
 #ifndef CLOCK_H_
 #define CLOCK_H_
 
+#include "system.h"
 
 #ifndef CLOCK_RATE
-	#define CLOCK_RATE 1000
+	#define CLOCK_RATE_HZ 1000
 #endif
 
+#define CLOCK_TIMER1_SIZE 65535
+// F_CPU is defined in system.h
+#define CLOCK_TIMER1_PRESCALER 64
+#define CLOCK_TIMER1_COMPARE_VALUE  (F_CPU / CLOCK_TIMER1_PRESCALER) / CLOCK_RATE_HZ
+/* tick rate = F_CPU / CLOCK_TIMER1_PRESCALER (ticks per second)
+ * compare value = tick rate (ticks per second) / clock rate (ticks per second)
+ *               = tick rate / clock rate (no units)
+ 
+   interrupt rate = tick rate (ticks per second) / compare value (ticks per interrupt)
+                  = tick rate / compare value (interrupts per second)
+ 
+    | prescaler	| compare value	|
+	|   1		| 8000			|
+	|	8		| 1000			|
+	|	64		| 125			|
+	|	256		| 31.25			|
+	|	1024	| 7.8125		|
+ */
 
 
 void clock_init(void);
 
-int clock_get_ticks(void);
+unsigned long clock_get_ms(void);
 
-int clock_get_ms(void);
+void clock_set_ms(unsigned long time);
 
-
-
+void clock_test(void);
 
 
 #endif /* CLOCK_H_ */
